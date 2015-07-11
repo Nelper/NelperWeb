617bee584710d07bfb4a8bb64ebe958a247a91a4
Janic Duplessis <janicduplessis@gmail.com>
2015-07-11 03:47:13 -0400

    fix release build, readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # NelperWeb
+
+## Installation
+
+```
+npm install
+```
+
+## Local dev server
+```
+npm run start
+```
+
+The site will be available at http://localhost:8080/
+
+## Production build
+```
+npm run build
+```
--- webpack.config.js
+++ webpack.config.js
@@ -26,34 +26,35 @@ var common = {
 if(TARGET === 'build') {
   module.exports = merge(common, {
     module: {
-      loaders: [
-        {
-          test: /\.scss$/,
-          loader: ExtractTextPlugin.extract('style', 'css', 'sass?sourceMap=true&includePaths[]=' +
-            path.resolve(__dirname, './node_modules')),
-        },
-        {
-          test: /\.jsx?$/,
-          loader: 'babel?stage=0',
-          include: path.resolve(ROOT_PATH, 'src'),
-        },
-      ],
-    },
-    plugins: [
-      new ExtractTextPlugin('styles.css'),
-      new webpack.DefinePlugin({
-        'process.env': {
-          // This has effect on the react lib size
-          'NODE_ENV': JSON.stringify('production'),
-        },
-      }),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false,
-        },
-      }),
+      loaders: [{
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass?sourceMap=true&includePaths[]=' +
+        path.resolve(__dirname, './node_modules')],
+      }, {
+        test: /\.jsx?$/,
+        loader: 'babel?stage=0',
+        include: path.resolve(ROOT_PATH, 'src'),
+      },
+      {test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
+      {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
     ],
-  });
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        // This has effect on the react lib size
+        'NODE_ENV': JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
+    new webpack.optimize.DedupePlugin(),
+  ],
+});
 }
 
 if(TARGET === 'dev') {
@@ -62,29 +63,23 @@ if(TARGET === 'dev') {
       'webpack/hot/dev-server',
     ],
     module: {
-      /*preLoaders: [
-        {
-          test: /\.css$/,
-          loader: 'csslint',
-        },
-        {
-          test: /\.jsx?$/,
-          loader: 'eslint-loader',
-          include: path.resolve(ROOT_PATH, 'app'),
-        },
-      ],*/
+      preLoaders: [{
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        include: path.resolve(ROOT_PATH, 'src'),
+      }],
       loaders: [{
-          test: /\.scss$/,
-          loaders: ['style', 'css', 'sass?sourceMap=true&includePaths[]=' +
-            path.resolve(__dirname, './node_modules')],
-        }, {
-          test: /\.jsx?$/,
-          loaders: ['react-hot', 'babel?stage=0'],
-          include: path.resolve(ROOT_PATH, 'src'),
-        },
-        {test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
-        {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
-      ],
-    },
-  });
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass?sourceMap=true&includePaths[]=' +
+        path.resolve(__dirname, './node_modules')],
+      }, {
+        test: /\.jsx?$/,
+        loaders: ['react-hot', 'babel?stage=0'],
+        include: path.resolve(ROOT_PATH, 'src'),
+      },
+      {test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
+      {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
+    ],
+  },
+});
 }
