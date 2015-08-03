cbe9e7a824565879434a8cabc7fc917cbe9d574c
Janic <janicduplessis@gmail.com>
2015-08-03 00:09:19 -0400

    Update design part 2

--- src/handlers/profile/ProfileHandler.js
+++ src/handlers/profile/ProfileHandler.js
@@ -35,7 +35,7 @@ export default class ProfileHandler extends Component {
               <div className="user-name">
                 {user.name}
               </div>
-              <Rating rating={4}/>
+              <Rating rating={4} dark={true}/>
               <div className="member-since">
                 Member since {moment(user.createdAt).format('MMMM Do YYYY')}
               </div>
