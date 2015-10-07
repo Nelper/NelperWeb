ea01749865ab348266beb4037b7d9efa36179827
Janic Duplessis <janicduplessis@gmail.com>
2015-10-07 18:05:08 -0400

    Update react to 0.14.0

--- src/server/template.js
+++ src/server/template.js
@@ -5,9 +5,9 @@ export default
     <meta charset="UTF-8">
     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
     <title>Nelper</title>
+    <link href="{styles}" rel="stylesheet" type="text/css">
     <link rel="shortcut icon" href="/favicon.ico">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,400italic,300,700" rel="stylesheet" type="text/css">
-    <link href="{styles}" rel="stylesheet" type="text/css">
   </head>
   <body>
     <div id="app" class="main-app">{content}</div>
