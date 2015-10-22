3554702279884064b8cdf5323a9239314dc42bbb
Janic Duplessis <janicduplessis@gmail.com>
2015-10-21 23:51:17 -0400

    Fix phone number format tests

--- src/utils/__tests__/IntlUtils-test.js
+++ src/utils/__tests__/IntlUtils-test.js
@@ -4,10 +4,10 @@ const IntlUtils = require('../IntlUtils');
 
 describe('IntlUtils', () => {
   it('returns the phone number with only numbers', () => {
-    expect(IntlUtils.cleanPhoneNumber('(123) 456-7890').toBe('1234567890'));
+    expect(IntlUtils.cleanPhoneNumber('(123) 456-7890')).toBe('1234567890');
   });
 
   it('returns the phone number formatted', () => {
-    expect(IntlUtils.cleanPhoneNumber('1234567890').toBe('(123) 456-7890'));
+    expect(IntlUtils.cleanPhoneNumber('1234567890')).toBe('(123) 456-7890');
   });
 });
